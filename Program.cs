4eab703bf89275f6d9c750abd5b631bd5e7e9e9b
Kusanagi-2029 <Sonyandpc87@gmail.com>
2020-10-28 12:36:38 +0300

    Added simple .NetCore App & Dockerfile

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace NetCore.Docker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n Please, input your first and middle name (example: Renat Maratovich):");
+            string name = Console.ReadLine();
+            Console.WriteLine(" Hello, dear interviewee {0}!", name);
+            Console.WriteLine("\n Please, input my first and last name (example: Renat Shakirov):");
+            string myName = Console.ReadLine();
+
+            if (myName.Equals("Renat Shakirov"))
+            {
+                Console.WriteLine(" \n                This is my GitHub:\n   https://github.com/Kusanagi-2029?tab=repositories");
+            }
+            else
+            {
+                Console.WriteLine("\n Please, input MY first and last name. EXAMPLE: \nRenat Shakirov\n :");
+                string myName2 = Console.ReadLine();
+
+                if (myName2.Equals("Renat Shakirov"))
+                {
+                    Console.WriteLine(" \n                This is my GitHub:\n   https://github.com/Kusanagi-2029?tab=repositories");
+                }
+                else
+                {
+                    Console.WriteLine("\n Oh, Jeez :(    here's an EXAMPLE: \nRenat Shakirov\n");
+                    string myName3 = Console.ReadLine();
+                    Console.WriteLine(" \n     -____-    okay, here is my   GitHub:\n  https://github.com/Kusanagi-2029?tab=repositories");
+                }
+            }
+        }
+    }
+}
